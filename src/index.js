774cb8b144884b60350b0ddd68e1b03db8ec2a9f
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2019-01-29 17:19:12 -0800

    set up for linking image src to dom

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import catOrDog from './cat-or-dog.js';
 const pickForm = document.getElementById('pick-form');
+const image = document.getElementById('animal-image');
 
 pickForm.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -7,8 +8,21 @@ pickForm.addEventListener('submit', function(event) {
     const randomNumber = Math.random();
     console.log(randomNumber);
 
-    //after we get to this point call catOrDog() no params and console.log it
+  //after we get to this point call catOrDog() no params and console.log it
     const animal = catOrDog(randomNumber);
     console.log(animal);
 
+    //time to navigate to the assets folder and go into how to make one
+
+    let imageSource = '';
+    if(animal === 'cat') {
+        imageSource = 'cat';
+    } else {
+        imageSource = 'dog';
+    }
+    //am i getting an image source
+    console.log(imageSource);
+
+    //set the dom node for image to have a source of imageSource
+    image.src = imageSource;
 });
